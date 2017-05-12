e58f1ba70cf0278369b5c88f7ad73096c1152d21
justineyeahyeah <justineyeah@163.com>
2017-05-12 11:21:27 +0800

    implement add_product_to_cart function

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,11 @@
 class Cart < ApplicationRecord
   has_many :cart_items
-  has_many :products, through: :cart_items, source: :products
+  has_many :products, through: :cart_items, source: :product
 
   def add_product_to_cart(product)
     ci = cart_items.build   #对has_many的关联的集合物件可以调用build和include这两个method
     ci.product = product
     ci.quantity = 1
     ci.save
+  end
 end
