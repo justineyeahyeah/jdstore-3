b3a69fb45451f601bc32533bd06317a8c6f2baca
justineyeahyeah <justineyeah@163.com>
2017-05-18 16:30:33 +0800

    create test data with seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,21 @@ u.password_confirmation = "123456"
 
 u.is_admin = true
 u.save
+
+
+f_title = ["clothes1", "clothes2", "clothes3", "clothes4", "clothes5", "clothes6"]
+f_images = ["http://wx3.sinaimg.cn/mw690/00658JkGgy1ffewfckzsyj30gj0gc401.jpg",
+  "http://wx3.sinaimg.cn/mw690/00658JkGgy1ffewfd98o6j30j60gnwg4.jpg",
+  "http://wx2.sinaimg.cn/mw690/00658JkGgy1ffewfcz57hj30j20gegmz.jpg",
+  "http://wx2.sinaimg.cn/mw690/00658JkGgy1ffewfdsef8j30he0ghmyn.jpg",
+  "http://wx3.sinaimg.cn/mw690/00658JkGgy1ffewe1dydsj30g60gm0u2.jpg",
+  "http://wx1.sinaimg.cn/mw690/00658JkGgy1ffewfe8gm0j30gu0gzq4j.jpg"]
+
+for i in 0..5 do
+  Product.create!(
+  title: f_title[i],
+  description: "clothes",
+  quantity:100,
+  price:rand(500..1000),
+  image: open(f_images[i]))
+end
