48510d63eff5af6e81c4efd3c616f8c9b46924b5
justineyeahyeah <justineyeah@163.com>
2017-05-19 09:13:48 +0800

    add apply_to_cancel function

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,10 +2,17 @@ class OrderMailer < ApplicationMailer
   def notify_order_placed(order)
     @order = order
     @user = order.user
-    @product_lists = order.product_lists
+    @product_lists = @order.product_lists
 
     mail(to: @user.email, subject: "[JDStore] 感谢您完成此次的订单，以下是您这次购物明细 #{order.token}")
   end
 
-  
+  def apply_cancel(order)
+    @order = order
+    @user = order.user
+    @product_lists = @order.product_lists
+
+    mail(to: "admin@test.com", subject: "[JDStore] 用户#{order.user.email}申请取消订单#{order.token}")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
     member do
       post :pay_with_alipay
       post :pay_with_wechat
+      post :apply_to_cancel
     end
   end
 
@@ -28,10 +29,12 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
     resources :orders do
+      member do
       post :cancel
       post :ship
       post :shipped
       post :return
+    end
     end
   end
 
