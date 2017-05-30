41b3b8e1abeeb357b6b032d7b1b374c753dfbac2
justineyeahyeah <justineyeah@163.com>
2017-05-30 12:01:02 +0800

    style shopping cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170526002553) do
-=======
-ActiveRecord::Schema.define(version: 20170524112234) do
-
-  create_table "addresses", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "address_type"
-    t.string   "contact_name"
-    t.string   "cellphone"
-    t.string   "address"
-    t.string   "zipcode"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["user_id", "address_type"], name: "index_addresses_on_user_id_and_address_type"
-  end
->>>>>>> account
-=======
-ActiveRecord::Schema.define(version: 20170526002553) do
->>>>>>> intercom
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -114,28 +94,12 @@ ActiveRecord::Schema.define(version: 20170526002553) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> intercom
     t.datetime "created_at",                                       null: false
     t.datetime "updated_at",                                       null: false
     t.boolean  "is_admin",                         default: false
     t.string   "address"
     t.string   "name"
     t.integer  "cellphone",              limit: 8
-<<<<<<< HEAD
-=======
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.boolean  "is_admin",               default: false
-    t.integer  "default_address_id"
-    t.string   "name"
-    t.string   "address"
-    t.integer  "phone_number"
->>>>>>> account
-=======
->>>>>>> intercom
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
