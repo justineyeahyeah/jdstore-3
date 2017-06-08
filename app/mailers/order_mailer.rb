ad0c8988ce6f376ccc27a52bdf58cf91eb0bbe81
justineyeahyeah <justineyeah@163.com>
2017-06-08 12:07:19 +0800

    fix bug

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,7 +4,7 @@ class OrderMailer < ApplicationMailer
     @user = order.user
     @product_lists = @order.product_lists
 
-    mail(to: @user.email, subject: "[JDStore] 感谢您完成此次的订单，以下是您这次购物明细 #{order.token}")
+    mail(to: @user.email, subject: "[ReadinEnglish] 感谢您完成此次的订单，以下是您这次购物明细 #{order.token}")
   end
 
   def apply_cancel(order)
@@ -12,7 +12,7 @@ class OrderMailer < ApplicationMailer
     @user = order.user
     @product_lists = @order.product_lists
 
-    mail(to: "admin@test.com", subject: "[JDStore] 用户#{order.user.email}申请取消订单#{order.token}")
+    mail(to: "admin@test.com", subject: "[ReadinEnglish] 用户#{order.user.email}申请取消订单#{order.token}")
   end
 
   def notify_ship(order)
@@ -20,7 +20,7 @@ class OrderMailer < ApplicationMailer
     @user = order.user
     @product_lists = @order.product_lists
 
-    mail(to: @user.email, subject: "[JDStore] 您的订单 #{order.token}已发货")
+    mail(to: @user.email, subject: "[ReadinEnglish] 您的订单 #{order.token}已发货")
   end
 
   def notify_cancel(order)
@@ -28,7 +28,7 @@ class OrderMailer < ApplicationMailer
     @user = order.user
     @product_lists = @order.product_lists
 
-    mail(to: @user.email, subject: "[JDStore] 您的订单 #{order.token}已取消")
+    mail(to: @user.email, subject: "[ReadinEnglish] 您的订单 #{order.token}已取消")
   end
 
   def notify_apply_to_return_good(order)
@@ -36,7 +36,7 @@ class OrderMailer < ApplicationMailer
     @user = order.user
     @product_lists = @order.product_lists
 
-    mail(to: "admin@test.com", subject: "[JDStore] 用户#{order.user.email}申请退货，订单号为#{order.token}")
+    mail(to: "admin@test.com", subject: "[ReadinEnglish] 用户#{order.user.email}申请退货，订单号为#{order.token}")
   end
 
   def notify_good_returned(order)
@@ -44,7 +44,7 @@ class OrderMailer < ApplicationMailer
     @user = order.user
     @product_lists = @order.product_lists
 
-    mail(to: @user.email, subject: "[JDStore] 您的订单 #{order.token}退货申请已通过")
+    mail(to: @user.email, subject: "[ReadinEnglish] 您的订单 #{order.token}退货申请已通过")
   end
 
   def notify_confirm_shipped(order)
@@ -52,7 +52,7 @@ class OrderMailer < ApplicationMailer
     @user = order.user
     @product_lists = @order.product_lists
 
-    mail(to: "admin@test.com", subject: "[JDStore] 用户#{order.user.email}已确认收货，订单号为#{order.token}")
+    mail(to: "admin@test.com", subject: "[ReadinEnglish] 用户#{order.user.email}已确认收货，订单号为#{order.token}")
   end
 
 
